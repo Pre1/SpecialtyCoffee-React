80ba3c40641936d172a96a4b632e5d7003f52f54
abdullah <anabanmi@hotmail.com>
2019-04-12 14:03:18 +0300

    check auth in private route

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
+import * as actionCreators from "../store/actions";
 
 class PrivateRoute extends Component {
+  async componentDidMount() {
+    console.log("TCL: PrivateRoute => componentDidMount");
+
+    await this.props.checkForExpiredToken();
+  }
+
   render() {
     const { component: Component, user, ...rest } = this.props;
+    console.log("TCL: PrivateRoute -> render -> user", user);
 
     return (
       <Route
         {...rest}
         render={props =>
-          user ? <Component {...props} /> : <Redirect to="/welcome" />
+          user ? <Component {...props} /> : <Redirect to="/login" />
         }
       />
     );
@@ -19,8 +27,17 @@ class PrivateRoute extends Component {
 
 const mapStateToProps = state => {
   return {
-    // user: state.profileReducer.user
+    user: state.profileReducer.user
   };
 };
 
-export default connect(mapStateToProps)(PrivateRoute);
+const mapDispatchToProps = dispatch => {
+  return {
+    checkForExpiredToken: () => dispatch(actionCreators.checkForExpiredToken())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PrivateRoute);
